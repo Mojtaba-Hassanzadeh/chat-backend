03021cc022d5a4c2bbce30a0c2f5cec01acca314
PlazaMac <PlazaMac@Gilertebat-Mac-mini.local>
2025-03-02 14:27:37 +0330

    feat : add base auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,21 @@
+// src/auth/auth.service.ts
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 10);
+  }
+
+  async validatePassword(password: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(password, hash);
+  }
+
+  async generateJwt(userId: string): Promise<string> {
+    return this.jwtService.sign({ userId });
+  }
+}
